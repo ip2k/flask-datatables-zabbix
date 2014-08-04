1e587ee66a945fdfb4e64d56724813f345080e5f
spetrow <spetrow@netflix.com>
2014-08-04 16:45:02 -0700

    Init

---
+++ server.py
@@ -0,0 +1,19 @@
+import pyzabbix
+from flask import Flask, jsonify, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/_get_hosts')
+def get_hosts():
+    pz = pyzabbix.ZabbixAPI(server='https://your-zabbix-server')
+    pz.login('username', 'password')
+    hosts = pz.host.get(output='extend', selectGroups='extend', selectInterfaces='extend')
+    return jsonify(data=hosts)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
